49c97d33df07f99316d1280d34b00e9cdc5f46f7
kubapi <kuba.sliz@hotmail.com>
2021-11-24 13:27:11 +0100

    modyfing scraper (csv format and processing on merge)

--- scripts/fact-checks-scraper.py
+++ scripts/fact-checks-scraper.py
@@ -27,32 +27,34 @@ def scrape_fakehunter():
 
     # checks if there is next page
     next_page_selector = "#__next  div > div > div.Container-sc-1gspdc4-0.cjdLzL > section > div.Pagination__PaginationWrapper-cr5t78-0.ephzDL > ul > li.next > a"   
-    while driver.find_element_by_css_selector(next_page_selector).get_attribute('aria-disabled') == 'false':
-        # wait for page to load
-        time.sleep(5)
-        
-        # returns all links with articles
-        for link in driver.find_elements_by_css_selector("a.ListItem__TitleLink-q70zzz-10"):
-            print(link.get_attribute('href'))
-            article_driver.get(link.get_attribute('href'))
-
-            # wait for article to load
-            time.sleep(3)
+    try:
+        while driver.find_element_by_css_selector(next_page_selector).get_attribute('aria-disabled') == 'false':
+            # wait for page to load
+            time.sleep(5)
             
-            temp_data = [link.get_attribute('href')]
-            selectors = ["h1.id__DetailsTitle-sc-131d89h-1", ".Verdict__VerdictStatus-sc-18ha1uh-1 p", "p.Typography__Text-wn1ri9-3", "p.d__StyledDate-sc-131d89h-7"]
-            for selector in selectors:
-                try:
-                    temp_data.append(article_driver.find_element_by_css_selector(selector).text)
-                except:
-                    temp_data.append('0')
-            data.append(temp_data)
+            # returns all links with articles
+            for link in driver.find_elements_by_css_selector("a.ListItem__TitleLink-q70zzz-10"):
+                print(link.get_attribute('href'))
+                article_driver.get(link.get_attribute('href'))
 
-        print("Next page!")
-        driver.execute_script("arguments[0].click();", driver.find_element_by_css_selector(next_page_selector))
+                # wait for article to load
+                time.sleep(3)
+                
+                temp_data = [link.get_attribute('href')]
+                selectors = ["h1.id__DetailsTitle-sc-131d89h-1", ".Verdict__VerdictStatus-sc-18ha1uh-1 p", "p.Typography__Text-wn1ri9-3", "p.id__StyledDate-sc-131d89h-7"]
+                for selector in selectors:
+                    try:
+                        temp_data.append(article_driver.find_element_by_css_selector(selector).text)
+                    except:
+                        temp_data.append('0')
+                data.append(temp_data)
+
+            print("Next page!")
+            driver.execute_script("arguments[0].click();", driver.find_element_by_css_selector(next_page_selector))
+    except:
+        pass
 
     driver.quit()
-
     return pd.DataFrame(data, columns=['url','title', 'verdict', 'short_justification', 'date'])
 
 def scrape_demagog():
@@ -67,31 +69,34 @@ def scrape_demagog():
     data = []
 
     # checks if there is next page
-    next_page_selector = "a.next"   
-    while driver.find_element_by_css_selector(next_page_selector):
-        #wait for page to load
-        time.sleep(5)
-        
-        # returns all links with articles
-        for link in driver.find_elements_by_css_selector("h2.title-archive a"):
-            print(link.get_attribute('href'))
-            article_driver.get(link.get_attribute('href'))
+    next_page_selector = "a.next" 
+    try:  
+        while driver.find_element_by_css_selector(next_page_selector):
+            #wait for page to load
+            time.sleep(5)
+            
+            # returns all links with articles
+            for link in driver.find_elements_by_css_selector("h2.title-archive a"):
+                print(link.get_attribute('href'))
+                article_driver.get(link.get_attribute('href'))
 
-            # wait for article to load
-            time.sleep(3)
+                # wait for article to load
+                time.sleep(3)
 
-            temp_data = [link.get_attribute('href')]
-            selectors = ["blockquote.hyphenate", "p.ocena", "div.date-content a"]
-            for selector in selectors:
-                try:
-                    temp_data.append(article_driver.find_element_by_css_selector(selector).text)
-                except:
-                    temp_data.append('0')
-            data.append(temp_data)
+                temp_data = [link.get_attribute('href')]
+                selectors = ["blockquote.hyphenate", "p.ocena", "div.date-content a"]
+                for selector in selectors:
+                    try:
+                        temp_data.append(article_driver.find_element_by_css_selector(selector).text)
+                    except:
+                        temp_data.append('0')
+                data.append(temp_data)
 
-        print("Next page!")
-        driver.execute_script("arguments[0].click();", driver.find_element_by_css_selector(next_page_selector))
-        
+            print("Next page!")
+            driver.execute_script("arguments[0].click();", driver.find_element_by_css_selector(next_page_selector))
+    except:
+        pass
+
     driver.quit()
     return pd.DataFrame(data, columns=['url','statement', 'verdict', 'date'])
 
@@ -110,31 +115,34 @@ def scrape_fakenews():
         time.sleep(5)
         
         # checks if there is next page
-        next_page_selector = "li.next"   
-        while driver.find_element_by_css_selector(next_page_selector):
-            #wait for page to load
-            time.sleep(5)
-            
-            # returns all links with articles
-            for link in driver.find_elements_by_css_selector(".post-title h2 a"):
-                print(link.get_attribute('href'))
-                article_driver.get(link.get_attribute('href'))
+        next_page_selector = "li.next a"
+        try:
+            while driver.find_element_by_css_selector(next_page_selector):
+                #wait for page to load
+                time.sleep(5)
+                
+                # returns all links with articles
+                for link in driver.find_elements_by_css_selector(".post-title h2 a"):
+                    print(link.get_attribute('href'))
+                    article_driver.get(link.get_attribute('href'))
 
-                # wait for article to load
-                time.sleep(3)
+                    # wait for article to load
+                    time.sleep(3)
 
-                temp_data = [link.get_attribute('href')]
-                selectors = [".title-post h1", ".verdict-wrapper .content h2", "div.subheader li"]
-                for selector in selectors:
-                    try:
-                        temp_data.append(article_driver.find_element_by_css_selector(selector).text)
-                    except:
-                        temp_data.append('0')
-                data.append(temp_data)
+                    temp_data = [link.get_attribute('href')]
+                    selectors = [".title-post h1", ".verdict-wrapper .content h2", "div.subheader li"]
+                    for selector in selectors:
+                        try:
+                            temp_data.append(article_driver.find_element_by_css_selector(selector).text)
+                        except:
+                            temp_data.append('0')
+                    data.append(temp_data)
 
-            print("Next page!")
-            driver.execute_script("arguments[0].click();", driver.find_element_by_css_selector(next_page_selector))
-        
+                print("Next page!")
+                driver.execute_script("arguments[0].click();", driver.find_element_by_css_selector(next_page_selector))
+        except:
+            continue
+
     driver.quit()
     return pd.DataFrame(data, columns=['url', 'title', 'verdict', 'date'])
 
@@ -142,11 +150,10 @@ if __name__ == '__main__':
     fakenews = scrape_fakenews()
     fakenews.to_csv('../database/fact-checks/fakenews.pl/'+generate_timestamp()+'.csv')
 
-    demagog = scrape_demagog()
-    demagog.to_csv('../database/fact-checks/demagog.org.pl/'+generate_timestamp()+'.csv')
-
     fakehunter = scrape_fakehunter()
     fakehunter.to_csv('../database/fact-checks/fakehunter.pap.pl/'+generate_timestamp()+'.csv')
 
-    fakenews = scrape_fakenews()
-    fakehunter.to_csv('../database/fact-checks/fakenews.pl/'+generate_timestamp()+'.csv')
+    demagog = scrape_demagog()
+    demagog.to_csv('../database/fact-checks/demagog.org.pl/'+generate_timestamp()+'.csv')
+
+    
--- scripts/scraper.py
+++ scripts/scraper.py
@@ -5,18 +5,22 @@ import twint
 import random
 import datetime
 
-def scrape_keyword(keyword):
+def scrape_keyword(keyword, from_day):
     """
-    Takes keyword and outputs dataframe of scraped tweets.
+    Takes keyword and outputs dataframe of scraped tweets
     """
+
     c = twint.Config()
     c.Lang = "pl"
     c.Limit = 5000
     c.Search = keyword
     c.Count = True
-    c.Stats = True   
-    c.Since = str((datetime.datetime.now() - datetime.timedelta(days=7)).date())
     c.Hide_output = True
+    c.Stats = True   
+    c.Since = str((datetime.datetime.now() - datetime.timedelta(days=from_day)).date())
+    if from_day >= 1:
+        c.Until = str((datetime.datetime.now() - datetime.timedelta(days=from_day-1)).date())
+
     
     # Set to True to display full user information. By default, only usernames are shown.
     c.User_full = True
@@ -27,19 +31,6 @@ def scrape_keyword(keyword):
     except:
         return False
     return twint.storage.panda.Tweets_df
-
-def scrape_user(username):
-
-    c = twint.Config()
-    c.Username = username
-    c.Pandas = True
-    c.Hide_output = True
-
-    try:
-        twint.run.Lookup(c)
-    except:
-        return False
-    return twint.storage.panda.User_df
 
 def generate_timestamp():
     """
@@ -53,34 +44,36 @@ def batch_run(keywords):
     """
     for e, keyword in enumerate(keywords):
         print(f"Scraping -> {keyword}")
-        # Adds to previous dataframe
-        if e == 0:
-            df = scrape_keyword(keyword)
-        else:
-            df = df.append(scrape_keyword(keyword), ignore_index=True,sort=False)
-    return df
+        for i in range(1,8):
+            # Adds to previous dataframe
+            if e == 0 and i == 1:
+                df = scrape_keyword(keyword, i)
+            else:
+                try:
+                    df = df.append(scrape_keyword(keyword, i), ignore_index=True,sort=False)
+                except:     
+                    continue
+    
+    # Deleting not polish tweets
+    df = df[df['language'] == 'pl']
 
-def batch_run_user(users):
-    """
-    Takes list of users and runs scrape_user() function on them. Outputs merged dataframe.
-    """
-    for e, user in enumerate(users):
-        print(f"Scraping -> {user}")
-        # Adds to previous dataframe
-        if e == 0:
-            df = scrape_user(user)
-        else:
-            df = df.append(scrape_user(user), ignore_index=True,sort=False)
+    # Deleting not important columns
+    df = df.drop(columns=['created_at','timezone','place','language','cashtags', 'retweet', 'quote_url', 'thumbnail', 'video', 'day', 'hour', 'near', 'geo', 'source', 'user_rt_id',
+                'user_rt', 'retweet_id','retweet_date','translate','trans_src','trans_dest', 'user_id'])
+
+    # Delete duplicates
+    df = df.drop_duplicates(subset = ['id'])
+
     return df
 
 def save_results(df):
     """
-    Takes dataframe and saves it to excel file with generated timestamp.
+    Takes dataframe and saves it to csv file with generated timestamp.
     """
-    df.to_excel("../database/"+generate_timestamp()+"-"+str(random.randint(0,10000))+".xlsx",engine='xlsxwriter', encoding = "utf-8")
+    df.to_csv("../database/"+generate_timestamp()+"-"+str(random.randint(0,10000))+".csv", encoding = "utf-8")
 
 if __name__ == "__main__":
-    df = batch_run(["segregacja", "covid1984", "nwo", "szczepimysie","szczepimysię","nieszczepimysię","segregację", "segregacji", "segregacją", "segregacji", "nieszczepimysie", "śmiertelna substancja", "śmiertelnej substancji", "śmiertelnej substancji", "śmiertelną substancją", "nop", "stopnop", "stop nop", "norymberga20", "zajob",  "plandemia", "plandemii", "plandemie", "plandemią", "niezaszczepiony", "niezaszczepiona", "niezaszczepioni", "niezaszczepieni", "niezaszczepię", "niezaszczepią", "nie zaszczepię", "nie zaszczepiona", "nie zaszczepiony", "nie zaszczepią", "zaszczepię", "zaszczepieni", "zaszczepiona", "zaszczepiony", "zaszczepią", "nieszczepimy", "szczepię", "szczepią", "szczepiona", "szczepieni", "szczepiony", "szczepionki", "szczeczepionek", "szczepionkom", "szczepionkami", "szczepionkach", "szczepionka", "szczepionki", "szczepionce", "szczepionką", "paszport covidowy", "paszportu covidowego", "paszportowi covidowemu", "paszportem covidowym", "paszporcie covidowym", "paszporty covidowe", "paszportów covidowych", "paszportom covidowym", "paszportami covidowymi", "paszportach covidowych", "certyfikat covid", "certyfikatu covid", "certyfikatowi covidowemu", "certyfikatem covidowym", "certyfikacie covidowym", "certyfikaty covidowe", "certyfikatów covidowych", "certyfikatom covidowym", "certyfikatami covidowymi", "certyfikatach covidowych"])
+    df = batch_run(["stopsegregacjisanitarnej","stopss","segregacja", "covid1984", "nwo", "szczepimysie","szczepimysię","nieszczepimysię","segregację", "segregacji", "segregacją", "segregacji", "nieszczepimysie", "śmiertelna substancja", "śmiertelnej substancji", "śmiertelnej substancji", "śmiertelną substancją", "nop", "stopnop", "stop nop", "norymberga20", "zajob",  "plandemia", "plandemii", "plandemie", "plandemią", "niezaszczepiony", "niezaszczepiona", "niezaszczepioni", "niezaszczepieni", "niezaszczepię", "niezaszczepią", "nie zaszczepię", "nie zaszczepiona", "nie zaszczepiony", "nie zaszczepią", "zaszczepię", "zaszczepieni", "zaszczepiona", "zaszczepiony", "zaszczepią", "nieszczepimy", "szczepię", "szczepią", "szczepiona", "szczepieni", "szczepiony", "szczepionki", "szczeczepionek", "szczepionkom", "szczepionkami", "szczepionkach", "szczepionka", "szczepionki", "szczepionce", "szczepionką", "paszport covidowy", "paszportu covidowego", "paszportowi covidowemu", "paszportem covidowym", "paszporcie covidowym", "paszporty covidowe", "paszportów covidowych", "paszportom covidowym", "paszportami covidowymi", "paszportach covidowych", "certyfikat covid", "certyfikatu covid", "certyfikatowi covidowemu", "certyfikatem covidowym", "certyfikacie covidowym", "certyfikaty covidowe", "certyfikatów covidowych", "certyfikatom covidowym", "certyfikatami covidowymi", "certyfikatach covidowych"])
 
     # Saves results to the file
     save_results(df)
