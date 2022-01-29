e5759d885440a787ce790428298683164c29d3b1
kubapi <kuba.sliz@hotmail.com>
2022-01-29 20:59:24 +0100

    Added duplicate removal when merging

--- processing/file_manager.py
+++ processing/file_manager.py
@@ -1,7 +1,16 @@
 import os
 import pandas as pd
 
-def merge_files(path: str):
+def merge_files(path: str, drop_duplicates_by: str = None) -> pd.DataFrame:
+    """Merges .csv files into a single DataFrame 
+
+    Args:
+        path (str): Path to the folder containing the .csv files
+        path (str): Element to drop duplicates by (eg. 'id', 'Username'). Deafult = None.
+
+    Returns:
+        [pd.DataFrame]: Returns merged DataFrame
+    """
     for subdir, dirs, files in os.walk(path):
         for e, file in enumerate(files):
             #print os.path.join(subdir, file)
@@ -15,3 +24,7 @@ def merge_files(path: str):
             else:
                 temp_df = pd.read_csv(filepath)
                 df = df.append(temp_df)
+    
+    if drop_duplicates_by != None:
+        df = df.drop_duplicates(subset = [drop_duplicates_by])
+    return df
